f0fcff55c119f3f83a08a80caeda39bb74a789ea
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:04:14 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "postgresql_flexible_server_firewall_rules_id" {
+  description = "Map of id values across all postgresql_flexible_server_firewall_rules, keyed the same as var.postgresql_flexible_server_firewall_rules"
+  value       = { for k, v in azurerm_postgresql_flexible_server_firewall_rule.postgresql_flexible_server_firewall_rules : k => v.id }
+}
 output "postgresql_flexible_server_firewall_rules_end_ip_address" {
   description = "Map of end_ip_address values across all postgresql_flexible_server_firewall_rules, keyed the same as var.postgresql_flexible_server_firewall_rules"
   value       = { for k, v in azurerm_postgresql_flexible_server_firewall_rule.postgresql_flexible_server_firewall_rules : k => v.end_ip_address }
