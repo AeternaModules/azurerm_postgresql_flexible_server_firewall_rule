eb30b761be5f1f878249115b66f6f132ab757568
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:43 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "postgresql_flexible_server_firewall_rules" {
-  description = "All postgresql_flexible_server_firewall_rule resources"
-  value       = azurerm_postgresql_flexible_server_firewall_rule.postgresql_flexible_server_firewall_rules
-}
 output "postgresql_flexible_server_firewall_rules_end_ip_address" {
-  description = "List of end_ip_address values across all postgresql_flexible_server_firewall_rules"
-  value       = [for k, v in azurerm_postgresql_flexible_server_firewall_rule.postgresql_flexible_server_firewall_rules : v.end_ip_address]
+  description = "Map of end_ip_address values across all postgresql_flexible_server_firewall_rules, keyed the same as var.postgresql_flexible_server_firewall_rules"
+  value       = { for k, v in azurerm_postgresql_flexible_server_firewall_rule.postgresql_flexible_server_firewall_rules : k => v.end_ip_address }
 }
 output "postgresql_flexible_server_firewall_rules_name" {
-  description = "List of name values across all postgresql_flexible_server_firewall_rules"
-  value       = [for k, v in azurerm_postgresql_flexible_server_firewall_rule.postgresql_flexible_server_firewall_rules : v.name]
+  description = "Map of name values across all postgresql_flexible_server_firewall_rules, keyed the same as var.postgresql_flexible_server_firewall_rules"
+  value       = { for k, v in azurerm_postgresql_flexible_server_firewall_rule.postgresql_flexible_server_firewall_rules : k => v.name }
 }
 output "postgresql_flexible_server_firewall_rules_server_id" {
-  description = "List of server_id values across all postgresql_flexible_server_firewall_rules"
-  value       = [for k, v in azurerm_postgresql_flexible_server_firewall_rule.postgresql_flexible_server_firewall_rules : v.server_id]
+  description = "Map of server_id values across all postgresql_flexible_server_firewall_rules, keyed the same as var.postgresql_flexible_server_firewall_rules"
+  value       = { for k, v in azurerm_postgresql_flexible_server_firewall_rule.postgresql_flexible_server_firewall_rules : k => v.server_id }
 }
 output "postgresql_flexible_server_firewall_rules_start_ip_address" {
-  description = "List of start_ip_address values across all postgresql_flexible_server_firewall_rules"
-  value       = [for k, v in azurerm_postgresql_flexible_server_firewall_rule.postgresql_flexible_server_firewall_rules : v.start_ip_address]
+  description = "Map of start_ip_address values across all postgresql_flexible_server_firewall_rules, keyed the same as var.postgresql_flexible_server_firewall_rules"
+  value       = { for k, v in azurerm_postgresql_flexible_server_firewall_rule.postgresql_flexible_server_firewall_rules : k => v.start_ip_address }
 }
 
